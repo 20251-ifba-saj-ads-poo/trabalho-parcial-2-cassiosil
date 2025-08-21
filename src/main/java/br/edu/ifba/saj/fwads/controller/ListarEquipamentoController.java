7a0a487136a59c5f1ccf68ecd4a6d34e2d6989db
Cassio Santos <20221tadssaj0016@ifba.edu.br>
2025-08-21 20:03:07 -0300

    v16

--- src/main/java/br/edu/ifba/saj/fwads/controller/ListarEquipamentoController.java
+++ src/main/java/br/edu/ifba/saj/fwads/controller/ListarEquipamentoController.java
@@ -1,30 +1,50 @@
+
 package br.edu.ifba.saj.fwads.controller;
 
+import br.edu.ifba.saj.fwads.App;
 import br.edu.ifba.saj.fwads.Dados;
 import br.edu.ifba.saj.fwads.model.Equipamento;
+import br.edu.ifba.saj.fwads.model.Status;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
-
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class ListarEquipamentoController {
+    @FXML
+    private TableView<Equipamento> tblEquipamento;
 
     @FXML
-    public  ListView<Equipamento> listaDeEquipamentos;
+    private TableColumn<Equipamento, String> columnNome;
+    @FXML
+    private TableColumn<Equipamento, String> columnNumeroDeSerie;
+    @FXML
+    private TableColumn<Equipamento, String> columnLocalizacao;
+    @FXML
+    private TableColumn<Equipamento, Status> columnStatus;
+
 
     @FXML
     public void initialize() {
-        listaDeEquipamentos.setItems(Dados.listaEquipamentos);
+        columnNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        columnNumeroDeSerie.setCellValueFactory(new PropertyValueFactory<>("numero de serie"));
+        columnLocalizacao.setCellValueFactory(new PropertyValueFactory<>("localização"));
+        columnStatus.setCellValueFactory(new PropertyValueFactory<>("localização"));
+        tblEquipamento.setItems(Dados.listaEquipamentos);
     }
-   
+
     @FXML
-    void removerEquipamento(MouseEvent event) {
-        int selectedID = listaDeEquipamentos.getSelectionModel().getSelectedIndex();
-        if(selectedID >= 0){
-            listaDeEquipamentos.getItems().remove(selectedID);
-        }
+    public void showNovoEquipamento() {
         
+        Stage stage = new Stage();            
+        Scene scene = new Scene(App.loadFXML("controller/CadEquipamentor.fxml"), 800, 600);            
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL); 
+        stage.showAndWait();            
     }
 
 }
-
